59aa6c3e561c82ddf9cbdf64f58cabf5f4f7f224
Vipul Kumar <vipul.ss.208@gmail.com>
2025-06-18 12:47:06 +0530

    Update production.py

--- holytrail/settings/production.py
+++ holytrail/settings/production.py
@@ -131,3 +131,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Email Backend
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = "587"
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+# EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
