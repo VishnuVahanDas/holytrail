5eab25b4db921d539261c742958e7b3fd4d0699d
Vipul Kumar <vipul.ss.208@gmail.com>
2025-06-24 03:51:08 +0530

    setting jazzmin

--- holytrail/settings/base.py
+++ holytrail/settings/base.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 import os
+from typing import Any, Dict
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__))))
@@ -46,15 +48,6 @@ INSTALLED_APPS = [
     "blog",
 
 ]
-JAZZMIN_SETTINGS = {
-    "site_title": "HolyTrail Admin",
-    "site_header": "HolyTrail Admin Portal",
-    "welcome_sign": "Welcome to HolyTrail Admin",
-    "copyright": "HolyTrail © 2025",
-    "search_model": "auth.User",
-    "show_ui_builder": True,
-    # add more options here if needed
-}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -114,6 +107,102 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+JAZZMIN_SETTINGS: Dict[str, Any] = {
+    
+    # Logo to use for your site, must be present in static files, used for brand on top left
+
+    # Relative path to logo for your site, used for login logo (must be present in static files. Defaults to site_logo)
+    "login_logo": "books/img/logo-login.png",
+    # Logo to use for login form in dark themes (must be present in static files. Defaults to login_logo)
+    "login_logo_dark": "books/img/logo-login-dark-mode.png",
+    # CSS classes that are applied to the logo
+    "site_logo_classes": None,
+    # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
+    "site_icon": "books/img/icon.png",
+    # List of model admins to search from the search bar, search bar omitted if excluded
+    # If you want to use a single search field you dont need to use a list, you can use a simple string
+    # Field name on user model that contains avatar ImageField/URLField/Charfield or a callable that receives the user
+    "user_avatar": None,
+    ############
+    # Top Menu #
+    ############
+    # Links to put along the top menu
+    "topmenu_links": [
+        # Url that gets reversed (Permissions can be added)
+        {"name": "Home", "url": "admin:index",
+            "permissions": ["auth.view_user"]},
+        # external url that opens in a new window (Permissions can be added)
+        # model admin to link to (Permissions checked against model)
+        {"model": "auth.User"},
+        # App with dropdown menu to all its models pages (Permissions checked against models)
+    ],
+    #############
+    # User Menu #
+    #############
+    # Additional links to include in the user menu on the top right ('app' url type is not allowed)
+    "usermenu_links": [
+        {"model": "auth.user"},
+    ],
+    #############
+    # Side Menu #
+    #############
+    # Whether to display the side menu
+    "show_sidebar": True,
+    # Whether to aut expand the menu
+    "navigation_expanded": True,
+    # Hide these apps when generating side menu e.g (auth)
+    "hide_apps": [],
+    # Hide these models when generating side menu (e.g auth.user)
+    "hide_models": [],
+    # List of apps to base side menu (app or model) ordering off of
+    "order_with_respect_to": ["Make Messages", "auth", "books", "books.author", "books.book", "loans"],
+    # Custom links to append to app groups, keyed on app name
+
+    # Custom icons for side menu apps/models See the link below
+    # https://fontawesome.com/icons?d=gallery&m=free&v=5.0.0,5.0.1,5.0.10,5.0.11,5.0.12,5.0.13,5.0.2,5.0.3,5.0.4,5.0.5,5.0.6,5.0.7,5.0.8,5.0.9,5.1.0,
+    # 5.1.1,5.2.0,5.3.0,5.3.1,5.4.0,5.4.1,5.4.2,5.13.0,5.12.0,5.11.2,5.11.1,5.10.0,5.9.0,5.8.2,5.8.1,5.7.2,5.7.1,5.7.0,5.6.3,5.5.0,5.4.2
+    # for the full list of 5.13.0 free icon classe
+
+    # Icons that are used when one is not manually specified
+    "default_icon_parents": "fas fa-chevron-circle-right",
+    "default_icon_children": "fas fa-circle",
+    #################
+    # Related Modal #
+    #################
+    # Activate Bootstrap modal
+    "related_modal_active": True,
+    #############
+    # UI Tweaks #
+    #############
+    # Relative paths to custom CSS/JS scripts (must be present in static files)
+    "custom_css": None,
+    "custom_js": None,
+    ###############
+    # Change view #
+    ###############
+    # Render out the change view as a single form, or in tabs, current options are
+    # - single
+    # - horizontal_tabs (default)
+    # - vertical_tabs
+    # - collapsible
+    # - carousel
+    "changeform_format": "horizontal_tabs",
+    # override change forms on a per modeladmin basis
+    "changeform_format_overrides": {"auth.user": "collapsible", "auth.group": "vertical_tabs"},
+
+}
+
+JAZZMIN_SETTINGS = {
+    "site_title": "HolyTrail Admin",
+    "site_header": "HolyTrail Admin Portal",
+    "welcome_sign": "Welcome to HolyTrail Admin",
+    "copyright": "HolyTrail © 2025",
+    "search_model": "auth.User",
+    "show_ui_builder": True,
+    # add more options here if needed
+}
+
 
 
 # Internationalization
